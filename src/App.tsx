f8daefb56d60ba1998cb438cf4704135cec64416
samir01 <samir.os.abdalla@gmail.com>
2024-01-06 14:15:52 -0800

    refactor: changed initial component that is loaded from root

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import ImageHandler from './components/ImageHandler/ImageHandler'
 
 function App() {
 
   return (
-    <div>
-
+    <div className="app__wrapper">
+      <h1>Choose an image to get started</h1>
+      <ImageHandler />
     </div>
   )
 }
