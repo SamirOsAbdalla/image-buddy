1e18b95f1021a648535941bedf0941c30c0d8609
samir01 <samir.os.abdalla@gmail.com>
2024-01-24 20:04:01 -0800

    refactor: fixed pixel replacement logic in floodfill button

--- src/components/FloodfillButton/FloodfillButton.tsx
+++ src/components/FloodfillButton/FloodfillButton.tsx
@@ -15,6 +15,8 @@ export default function FloodfillButton({
     setCtx
 }: Props) {
 
+    let replacedColorArr = replacedColor.substring(5, replacedColor.length - 1).split(",").map(pixelVal => parseInt(pixelVal))
+    let replacementColorArr = replacementColor.substring(5, replacementColor.length - 1).split(",").map(pixelVal => parseInt(pixelVal))
     function replaceImageColor() {
         if (!replacedColor || !replacementColor) {
             return
@@ -25,20 +27,20 @@ export default function FloodfillButton({
         const { data } = image;
         const { length } = data;
 
+
+
         for (let i = 0; i < length; i += 4) {
             const r = data[i + 0];
             const g = data[i + 1];
             const b = data[i + 2];
             const a = data[i + 3];
 
-            console.log(g)
-            if (g > 100) {
-
-                data[i] = 255;
-                data[i + 1] = 255 // green is set to 100%
-                data[i + 2] = 255; // blue is set to 100%
-                // resulting color is white
+            if (Math.abs(replacedColorArr[0] - r) < 10 && Math.abs(replacedColorArr[1] - g) < 10 && Math.abs(replacedColorArr[2] - b) < 10) {
+                data[i] = replacementColorArr[0]
+                data[i + 1] = replacementColorArr[1]
+                data[i + 2] = replacementColorArr[2]
             }
+
         }
 
         ctx.putImageData(image, 0, 0)
